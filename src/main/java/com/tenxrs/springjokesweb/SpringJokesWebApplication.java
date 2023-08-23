44a545a279f01b2e4f32d052400a2c0e088dcdcb
vmpenyana <vincent@tenxrs.com>
2023-08-23 08:17:42 +0200

    Initialize project

---
+++ src/main/java/com/tenxrs/springjokesweb/SpringJokesWebApplication.java
@@ -0,0 +1,13 @@
+package com.tenxrs.springjokesweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJokesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJokesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenxrs/springjokesweb/SpringJokesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenxrs.springjokesweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
